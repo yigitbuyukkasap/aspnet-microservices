3888abd1f4500edd43deb8f32a8e3ff250c0c2b9
yigitbuyukkasap <yigitbuyukkasap@gmail.com>
2022-04-13 22:00:41 +0300

    Configure Consumer in Ordering.APİ

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,17 @@
+﻿using EventBus.Messages.Events;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckOutEvent>
+    {
+        public Task Consume(ConsumeContext<BasketCheckOutEvent> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
